891717b57d3f6e6e46a2bc318614af322e33bd45
Weslley Lima <weslley.lima@zup.com.br>
2021-03-04 15:32:45 -0300

    Criar classe Pessoas com os atributos get e set e construtor

---
+++ src/main/java/br/com/zup/cadastro/model/Pessoa.java
@@ -0,0 +1,52 @@
+package br.com.zup.cadastro.model;
+
+import java.util.Date;
+
+public class Pessoa {
+    private String userName;
+    private String cpf;
+    private Date dataNascimento;
+    private String senha;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String userName, String cpf, Date dataNascimento, String senha) {
+        this.userName = userName;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
